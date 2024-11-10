af50d3aff4ed897b2aa0f637668ea28ae2bdb7e5
Amama Rida <74170920+ridamaqdoom@users.noreply.github.com>
2024-11-09 21:28:06 -0600

    Adding the code to get the length of last word in string

--- Length_of_Last_Word.java
+++ Length_of_Last_Word.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    public int lengthOfLastWord(String s) {
+        // Remove the space from the words 
+        s = s.trim();
+        // Split the string by spaces
+        String[] returning_word = s.split(" ");
+        // Return the length of last element.
+        return returning_word[returning_word.length-1].length();
+    
+    }
+}
