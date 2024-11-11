63dd346d7a598e956b635a17114df8080baa18bb
Amama Rida <74170920+ridamaqdoom@users.noreply.github.com>
2024-11-11 09:18:48 -0600

    Adding code to  Find_index_of_first_occurance.java

--- Find_index_of_first_occurance.java
+++ Find_index_of_first_occurance.java
@@ -1,1 +1,20 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        // length of needle 
+        int x = needle.length();
+        // length of haystack 
+        int y = haystack.length();
+       //check each substring of equal length to needle.
+       // when substring haystack macthes needle retuen index i
+        for (int i=0; i<=y-x; i++){
+            if (haystack.substring(i,i+x).equals(needle)){
+                return i;
+                }}
+        //else reurn -1
+        return -1;
+            }
+        
+}
+        
+    
 
