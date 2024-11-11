d4dc8c48c3f5c48de862eea7e7a00587b2b7cfcc
Amama Rida <74170920+ridamaqdoom@users.noreply.github.com>
2024-11-11 09:35:39 -0600

    Adding solution to  Search_insert_position.java

--- Search_insert_position.java
+++ Search_insert_position.java
@@ -1,1 +1,30 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        // intialize left to index 0
+        int left = 0;
+        // right is length of the nums
+        int right = nums.length -1;
 
+        // when left is less than right 
+        while(left <= right){
+            // intialize mid as the middle value from left and right 
+            int mid = left + (right - left)/2;
+            // if num ad mid == target return index
+            if (nums[mid] == target){
+                return mid;
+            }
+            // if mid is greater than target move right 
+            if(nums[mid] > target){
+                right = mid -1;
+            }
+            // if mid is less than target move left 
+
+            else {
+                left = mid +1;
+            }
+                    }
+        // else return left 
+        return left;
+
+    }}
+        
